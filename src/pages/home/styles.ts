1a460b7ef698d1e25baa2844a8cf815c5555250b
Carlos Alisson <carlosalisson01@gmail.com>
2025-05-15 23:09:45 -0300

    fiz correct

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,15 +1,36 @@
-import { styled } from '@ignite-ui/react'
+import { styled, Heading, Text } from '@ignite-ui/react'
 
 export const Container = styled('div', {
-    with: 1200,
-    margin: 'auto',
-    background: 'red',
+    maxWidth: 'calc(100vw -  (100vw - 1160px) / 2)',
+    marginLeft: 'auto',
+    height: '100vh',
     display: 'flex',
     alignItems: 'center',
-    gap: '20',
+    gap: '$20',
+    overflow: 'hidden',
     
 })
 
-export const Hero = styled('div', {})
+export const Hero = styled('div', {
+    maxWidth: 480,
+    padding: '0 $10',
 
-export const Preview = styled('div', { })
+   [`${Heading}`]: {
+     '@media (max-width: 600px)': {
+        fontSize: '$6xl',
+     },
+   },
+   [`${Text}`]: {
+    marginTop: '$2',
+    color: '$gray200',
+    fontSize: '$xl',
+   },
+})
+
+export const Preview = styled('div', { 
+    paddingRight: '$8',
+    overflow: 'hidden',
+    '@media (max-width: 600px)': {
+        display: 'none',
+    },
+})
