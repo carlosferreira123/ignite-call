7245fb8cd171ff67cc0aa6065f1f0894e4414bb8
Carlos Alisson <carlosalisson01@gmail.com>
2025-05-19 12:32:53 -0300

    commit style

---
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -0,0 +1,18 @@
+import { Button, TextInput } from "@ignite-ui/react";
+import { Form, } from "./styles";
+
+export function ClaimUsernameForm() {
+  return (
+    <Form>
+        <TextInput
+         size="sm"
+         prefix="ignite.com/"
+         placeholder="seu-usuario"
+        />
+        <Button 
+         size="sm" type="submit">
+           Reservar usuario 
+            
+        </Button>
+    </Form>
+}
